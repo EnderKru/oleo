5def9e2afb33f63fbfbad09aca13807efd8e0b2a
EnderKru <enderpro700@gmail.com>
2023-12-03 23:36:43 +0300

    first components

---
+++ src/components/leaves.jsx
@@ -0,0 +1,11 @@
+import '../styles/leaves.css'
+
+const leaves = ()=>{
+    return (
+        <div className='background-leaves'>
+            {}
+        </div>
+    )
+}
+
+export default leaves
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+
+import './styles/common.css';
+import '../src/styles/reset.css';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
