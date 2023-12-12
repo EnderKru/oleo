b669f6af50801e46da034e7a6716e40d0c372c89
EnderKru <enderpro700@gmail.com>
2023-12-12 08:02:08 +0300

    nopage

--- src/pages/noPage/noPage.jsx
+++ src/pages/noPage/noPage.jsx
@@ -2,7 +2,15 @@ import "../noPage/noPage.css"
 export function NoPage() {
     return(
         <div className="no-page-body">
-            Oops, We can't find that page.
+            <div className="error">
+                Oops, We can't find that page... Oops, We can't find that page... Oops, We can't find that page...
+            </div>
+            <div className="donate-img">
+                <img src="/src/assets/images/donate.svg" alt="donate" />
+            </div>
+            <div className="error">
+                Oops, We can't find that page... Oops, We can't find that page... Oops, We can't find that page...
+            </div>
         </div>
     )
 }
