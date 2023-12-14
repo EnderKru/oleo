9e52a8eccc2373e0ee7f86604882a7ffaa6c8c81
EnderKru <enderpro700@gmail.com>
2023-12-14 16:08:58 +0300

    kg

--- src/app.jsx
+++ src/app.jsx
@@ -2,9 +2,11 @@ import React, { useState } from 'react'
 import {RouterProvider} from 'react-router-dom'
 import {router} from './app/index'
 
+
 const App = () => {
  
     return <RouterProvider router={router} />
   }
   
   export default App
+
