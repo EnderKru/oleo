a76d92dc2a9dfe1969f44c508e61144468491979
EnderKru <enderpro700@gmail.com>
2023-12-17 18:13:51 +0300

    accidentally started making a profile page here

---
+++ src/pages/profilePage/profile-header/profile-header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import classes from './profile-header.module.css'
+
+export function ProfileHeader() {
+  return (
+    <div className={classes.header}>
+        gsgs
+    </div>
+  )
+}
