f91eeb4f01fefdc90c17a00493985a5e7d191d8d
EnderKru <enderpro700@gmail.com>
2023-12-19 07:52:15 +0300

    styles update

---
+++ src/pages/cookPage/cook-header/cook-header.jsx
@@ -0,0 +1,35 @@
+import { NavLink } from "react-router-dom"; 
+
+export function CookHeader(){
+    return(
+        <header>
+            <div style={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center'
+                
+                }}>
+                            <NavLink to="/">
+                    <img src="./src/assets/фото/logo.png" className="logo-cook" alt="Logo" />
+                    </NavLink>
+                    <div className="custom-tooltip-cook">
+                        <img
+                        src="../src/assets/фото/image 8person.png"
+                        alt="Mini Photo"
+                        className="mini-photo-cook"
+                        id="person-cook"
+                        />
+                        <div className="tooltiptext-cook">
+                        <a href="http://localhost:5173/registration" className="sign-up-cook" >
+                            sign up
+                        </a>
+                        <a href="http://localhost:5173/login" className="log-in-cook" >
+                            log in
+                        </a>
+                        </div>
+                    </div>
+                   
+            </div>
+        </header>
+    )
+}
