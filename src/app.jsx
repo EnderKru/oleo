8bf97300b915a24739157f83390950685eec8e7b
manziro785 <manziro785@gmail.com>
2023-12-19 17:03:49 +0600

    Arthur Agai helped

--- src/app.jsx
+++ src/app.jsx
@@ -1,17 +1,11 @@
-import React, { useState, createContext} from 'react' 
+import React, { useState } from 'react' 
 import {RouterProvider} from 'react-router-dom'
 import {router} from './app/index'
 
-export const UserContext = createContext()
 
 const App = () => {
-  const [user, setUser] = useState(false)
  
-    return (
-      <UserContext.Provider value={[user, setUser]}>
-        <RouterProvider router={router} />
-      </UserContext.Provider>
-    )
+    return <RouterProvider router={router} />
   }
   
   export default App
